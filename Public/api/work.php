26fa354a5b34d98feacb2564063b3e820115b462
jdkinzel <jdkinzel@indiana.edu>
2018-10-01 10:12:48 -0400

    Class 10/1

--- Public/api/work.php
+++ Public/api/work.php
@@ -1,13 +1,24 @@
 <?php
 
+require '../../app/common.php';
 
-// get the taskId from URL params
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+  require 'workPost.php';
+  exit;
+}
+
 $taskId = intval($_GET['taskId'] ?? 0);
 
-// Fetch the work from the // db
-$work = Work::findByTaskId($taskId);
+if ($taskId < 1) {
+  throw new Exception('Invalid Task ID');
+}
 
-// convert to json and print
-echo json_encode($work);
+// 1. Go to the database and get all work associated with the $taskId
+$workArr = Work::getWorkByTaskId($taskId);
 
-?>
+// 2. Convert to JSON
+$json = json_encode($workArr, JSON_PRETTY_PRINT);
+
+// 3. Print
+header('Content-Type: application/json');
+echo $json;
---
+++ Public/api/workPost.php
@@ -0,0 +1,6 @@
+<?php
+require_once '../../common.php';
+
+var_dump($_POST);
+
+ 
